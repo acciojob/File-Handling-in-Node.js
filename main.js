149986eb04bf080dcdde8221173cb7dd97037c29
thelastenvoy <satvikbatra@gmail.com>
2023-11-05 15:31:24 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-const https = require('https');
+const fs = require('fs');
+const csv = require('csv-parser');
 
-// TODO: Implement this function
+const csvFilePath = process.argv[2];
+const columnName = process.argv[3];
+
+let sum = 0;
+
+fs.createReadStream(csvFilePath)
+  .pipe(csv())
+  .on('data', (data) => {
+    // TODO: Add code to sum values in the specified column
+  })
+  .on('end', () => {
+    console.log(`The sum of ${columnName} is: ${sum}`);
+  });
